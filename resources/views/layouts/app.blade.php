a45d0fe1baf77874cf8daa015e8341c0454b4148
marsislav <mkostadinov87@gmail.com>
2023-12-13 12:26:40 +0200

    Fixed bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <title>@yield('title', config('app.name', 'mCMS 4.1'))</title>
+        <meta name="description" content="@yield('meta_description', 'mCMS 4.1 - Free CMS by marsislav')" />
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
